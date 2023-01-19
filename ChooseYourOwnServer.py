c6869734195eb23e2d49ff4def5a57f09c00a9b0
Brian Scanlon <brian_scanlon@hotmail.co.uk>
2023-01-19 18:09:03 +0000

    update

--- ChooseYourOwnServer.py
+++ ChooseYourOwnServer.py
@@ -15,14 +15,28 @@ class ChooseYourOwnServer:
 
     def constructBook(self, title=None):
         if not title:
-            page1 = Page(1, "The first page")
-            page2 = Page(2, "The second page")
+            page1 = Page(1, """Page 1
+            =================Escape the Dungeon!===================
+            You awake.  Finally.  With a sense of increasing dread and fear, you cannot remember anything
+            that came before this moment.  All you know is that you are lying on a cool, dry stone floor.
+            You sit up and look around.  Your in a square room around 5 metres square.  The floor is made of well
+            fitting, smooth flagstones. The walls are of large stone blocks, the same material as the floor.
+            There is a crude, wooden table in the middle of the room with a number of objects on it.
+            There appear to be two exits, one in front of you, which you guess at being towards the north and one to
+            left of you, maybe towards the east.
+            """)
+            page2 = Page(2, """Page 2
+            =================The Crude Table===================
+            
+            """)
             page3 = Page(3, "The Third page")
 
-            page1.add_option(Option(2, "Move to the second page"))
-            page1.add_option(Option(3, "Move to the third page"))
+            page1.add_option(Option(2, "Examine the table, move to page 2"))
+            page1.add_option(Option(3, "Go through the north door, move to page 3"))
+            page1.add_option(Option(4, "Go through the east door, move to page 4"))
             page2.add_option(Option(3, "Move to the third page"))
             page3.add_option(Option(1, "Move to the first page"))
+
 
             self.book.add_page(page1)
             self.book.add_page(page2)
